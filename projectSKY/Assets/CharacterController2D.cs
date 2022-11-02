7b06e17bb3d5a6837d64bdf285d519ecdee82e54
bebguin <82475058+bebguin@users.noreply.github.com>
2022-11-02 21:55:14 +0000

    files

---
+++ projectSKY/Assets/CharacterController2D.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class CharacterController2D : MonoBehaviour
+{
+    Rigidbody2D rigidbody2D;
+    [SerializeField] float speed = 2f;
+    Vector2 motionVector;
+
+    void Awake()
+    {
+        rigidbody2D = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        motionVector = new Vector2(
+            Input.GetAxisRaw("Horizontal"),
+            Input.GetAxisRaw("Vertical")
+            );
+    }
+
+    void fixedUpdate()
+    {
+        Move();
+
+    }
+
+    private void Move()
+    {
+        rigidbody2D.velocity = motionVector * speed;
+    }
+}
